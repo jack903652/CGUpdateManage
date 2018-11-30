0ad0b1393f20f564153ddd0851ed1bb1998bd697
bamq <you@example.com>
2018-11-30 18:31:47 +0800

    支持carthage

---
+++ CGUpdateManageKit/CGUpdateManageKit.h
@@ -0,0 +1,19 @@
+//
+//  CGUpdateManageKit.h
+//  CGUpdateManageKit
+//
+//  Created by bamq on 2018/11/30.
+//  Copyright © 2018 bamq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CGUpdateManageKit.
+FOUNDATION_EXPORT double CGUpdateManageKitVersionNumber;
+
+//! Project version string for CGUpdateManageKit.
+FOUNDATION_EXPORT const unsigned char CGUpdateManageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGUpdateManageKit/PublicHeader.h>
+
+
