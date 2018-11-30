4144a3603e0365cc06f505d9156b9032970604c7
bamq <you@example.com>
2018-11-30 18:50:56 +0800

    framework 修改import 文件

--- CGUpdateManageKit/CGUpdateManageKit.h
+++ CGUpdateManageKit/CGUpdateManageKit.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char CGUpdateManageKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CGUpdateManageKit/PublicHeader.h>
 
-
+#import <CGUpdateManageKit/UpdateManage.h>
